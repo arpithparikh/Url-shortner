85c98df0b9e8e91bd61657c70faf240603d08838
System Administrator <arpithparikh@gmail.com>
2017-04-06 14:04:04 -0400

    Url Shortner

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {};
+//Db Config
+config.db = {};
+
+//webhost!
+config.webhost = 'https://agnes.io/';
+
+config.db.host = '34.200.166.75';
+config.db.name = 'url_shortener';
+
+module.exports = config;
